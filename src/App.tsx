eb65acefc0455c2b570ed7a3395282a9a9501fdf
Sehani <iit18034@std.uwu.ac.lk>
2023-05-23 16:13:42 +0530

    Test and code button click

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import React, { useState } from 'react';
+
+
+function App() {
+  const [ buttonColor, setButtonColor ] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red' ;
+  
+  return (
+    <div>
+      <button 
+      style={{ backgroundColor: buttonColor}}
+      onClick={() => setButtonColor(newButtonColor)}
+      >
+         Change to {newButtonColor} 
+      </button>
+    </div>
+  );
+}
+
+export default App;
