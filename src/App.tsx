676417f0718b7720c4d98a166c92b060f14a97bb
Sehani <iit18034@std.uwu.ac.lk>
2023-05-25 12:35:51 +0530

    secondco

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import "./App.css";
-import React, { useState } from 'react';
+import { useState } from "react";
 
+export function replaceCamelWithSpaces(colorName:String) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('red');
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red' ;
-  const [disabled,setdisabled] = useState(false);
+  const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button 
-      style={{ backgroundColor: buttonColor}}
-      onClick={() => setButtonColor(newButtonColor)}
-      disabled={disabled}
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
-         Change to {newButtonColor} 
+        Change to {newButtonColor}
       </button>
-      <input 
-      type="checkbox"
-      id="enable-button-checkbox"
-      defaultChecked={disabled}
-      onChange={(e) => setdisabled(e.target.checked)}
-    />
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
